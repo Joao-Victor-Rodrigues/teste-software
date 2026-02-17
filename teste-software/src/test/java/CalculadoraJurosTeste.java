02f2af749f3d9160078749bf6f5f4b982d53deff
jvrodrigues <joao.victor.rodrigues@philips.com>
2026-02-17 19:32:29 -0300

    [SO-000001] Fazendo os testes darem erro

--- teste-software/src/test/java/CalculadoraJurosTeste.java
+++ teste-software/src/test/java/CalculadoraJurosTeste.java
@@ -5,74 +5,53 @@ public class CalculadoraJurosTeste {
 
     @Test
     void calcularJurosSimples() {
-        final double MONTANTE_ESPERADO = 1.100;
+        final double MONTANTE_ESPERADO = 1100.0;
 
-        double capital = 1.000;
-        double taxaJuros = 0.01;
-        int tempo = 1;
-
-        double juros = capital * taxaJuros * tempo;
-        double montante = capital + juros;
+        CalculadoraJuros calculadora = new CalculadoraJuros();
+        double montante = calculadora.calcularJurosSimples(1000.0, 0.01, 1);
 
         assertEquals(MONTANTE_ESPERADO, montante);
     }
 
-    // Cenário 2
     @Test
     void deveRetornarZeroQuandoCapitalForZero() {
         final double MONTANTE_ESPERADO = 0.0;
-        double capital = 0.0;
-        double taxaJuros = 0.01;
-        int tempo = 5;
 
-        double juros = capital * taxaJuros * tempo;
-        double montante = capital + juros;
+        CalculadoraJuros calculadora = new CalculadoraJuros();
+        double montante = calculadora.calcularJurosSimples(0.0, 0.01, 5);
 
         assertEquals(MONTANTE_ESPERADO, montante);
     }
 
-    // Cenário 3
     @Test
     void deveManterMesmoValorQuandoTaxaForZero() {
-        final double MONTANTE_ESPERADO = 500.00;
-        
-        double capital = 500.0;
-        double taxaJuros = 0.0;
-        int tempo = 10;
+        final double MONTANTE_ESPERADO = 500.0;
 
-        double juros = capital * taxaJuros * tempo;
-        double montante = capital + juros;
+        CalculadoraJuros calculadora = new CalculadoraJuros();
+        double montante = calculadora.calcularJurosSimples(500.0, 0.0, 10);
 
         assertEquals(MONTANTE_ESPERADO, montante);
     }
 
-    // Cenário 4
     @Test
     void deveNegarCalculoQuandoTempoForNegativo() {
-        final String EXCEPTION_MESSAGE = "Tempo inválido";
-        int tempo = -2;
+        CalculadoraJuros calculadora = new CalculadoraJuros();
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            if (tempo < 0) {
-                throw new IllegalArgumentException("Tempo inválido");
-            }
+            calculadora.calcularJurosSimples(1000.0, 0.01, -2);
         });
 
-        assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
+        assertEquals("Tempo inválido", exception.getMessage());
     }
 
-    // Cenário 5
     @Test
     void deveNegarCalculoQuandoCapitalForNegativo() {
-        final String EXCEPTION_MESSAGE = "Valor inválido";
-        double capital = -1000.0;
+        CalculadoraJuros calculadora = new CalculadoraJuros();
 
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
-            if (capital < 0) {
-                throw new IllegalArgumentException("Valor inválido");
-            }
+            calculadora.calcularJurosSimples(-1000.0, 0.01, 2);
         });
 
-        assertEquals(EXCEPTION_MESSAGE, exception.getMessage());
+        assertEquals("Valor inválido", exception.getMessage());
     }
 }
